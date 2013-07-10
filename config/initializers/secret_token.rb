60f4ebedce01f3133db9c7737f2259003302544d
Ryosuke Yamazaki <ryosuke.yamazaki@mac.com>
2013-07-11 06:22:50 +0900

    揚げ足取られ防止

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,16 @@
+# coding: utf-8
+
 # Be sure to restart your server when you modify this file.
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+
+
+# 注記:
+#   ここの値は public な場所に晒してよいものではありません。
+#   通常の Rails アプリケーションのソースを公開の場所に設置する場合には
+#   気をつけましょう
+
 PleaseStrongify::Application.config.secret_token = '09b9bf3806680e37d2edb0a468bf5ac42531142a5863d8b0b383c552bdf5a38373e2c30ba3e72968ff8972f13a3f50f8637ccd8d39f2fbb3f911cec14b272aee'
