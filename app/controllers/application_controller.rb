4f7028997c252347c558123c93d485255ebbbcfc
ryosukey <ryosukey@10.200.35.22.d.tokyo.iiji.jp>
2013-07-11 09:54:48 +0900

    do not show stack traces.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+
+  def not_found
+    render :nothing => true, :status => :not_found
+  end
 end
